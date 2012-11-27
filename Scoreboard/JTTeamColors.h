e82c591c0d1f4da9463e6d4030abcf4e6696ecc3
Joel <barebonesgraphics@gmail.com>
2012-11-27 03:12:33 -0500

    made JTTeamColors much more palatable

--- Scoreboard/JTTeamColors.h
+++ Scoreboard/JTTeamColors.h
@@ -8,25 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum {
-    Beez,
-    Duckies,
-    Sharks,
-    Rats,
-    Frogs,
-    Nights,
-    FFF,
-    Pirates,
-    m0nstars,
-    Roos,
-    Nobody = -1
-} PHLTeam;
-
 @interface JTTeamColors : NSObject
 
 + (NSColor *)hexToColor:(NSString *)s;
-+ (NSColor *)colorForTeam:(PHLTeam)t;
-+ (NSColor *)foregroundColorForTeam:(PHLTeam)t;
-+ (NSString *)nameForTeam:(PHLTeam)t;
 
 @end
